b4954d7287d04a2e9b3dc53bbf6d6a5f24155885
Youngermaster <jmyoungh@eafit.edu.co>
2019-10-17 19:51:15 -0500

    I change the AntoniaSkill class

--- __init__.py
+++ __init__.py
@@ -6,13 +6,6 @@ from mycroft.skills.audioservice import AudioService
 import json # This library make us able to convert text to json.
 import os # This library make us be able to execute Os commands
 
-JSON_PATH = 'assets/json/request.json'
-
- # This will be the json to send to the server.
-jsonTest = {
-    "user": "Antonia",
-}
-
 class AntoniaSkill(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor.
@@ -20,6 +13,11 @@ class AntoniaSkill(MycroftSkill):
         super(AntoniaSkill, self).__init__("AntoniaSkill")
         # Initialize working variables used within the skill.
         self.count = 0
+        # This will be the json to send to the server.
+        self.jsonTest = {
+            "user": "Antonia",
+        }
+        self.JSON_PATH = 'assets/json/request.json'
 
     def initialize(self):
         # Creating I have a question intent.
@@ -30,28 +28,28 @@ class AntoniaSkill(MycroftSkill):
         
     @intent_handler(IntentBuilder("").require("IHaveAQuestion"))
     def handle_i_have_a_question_intent(self, message):
-        
         question = self.get_response(dialog="i.have.a.question")
         add_atributes_to_json(question)
         generate_json()
         execute_curl('request.json', 'https://projectantonia.ngrok.io/test/message')
        #added:
-        generate_mp3(self, "/home/pi/answer")
+        play_mp3("/home/pi/answer/answer.mp3")
         
     def add_atributes_to_json(self, request):
-        jsonTest["text"] = request
+        self.jsonTest["text"] = request
 
     def generate_json(self):
-        with open(JSON_PATH, 'w') as outfile:
+        with open(self.JSON_PATH, 'w') as outfile:
             json.dump(jsonTest, outfile)
 
     def execute_curl(self, jsonName, tunnelUrl):
         os.system('curl -H "Content-Type: application/json" -d @assets/json/' + jsonName + ' ' + tunnelUrl)
 
-    def generate_mp3(self, audio_path):
+    def play_mp3(self, audio_path):
         while os.path.exist(audio_path):
             self.audio_service.play(audio_path)
             os.remove("answer.mp3")
+
         
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
